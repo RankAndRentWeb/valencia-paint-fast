03df9cb85b5f9135d1b80a90cb27fa48884ebfff
Edgar7222 <1745554@uab.cat>
2025-10-06 01:32:45 +0200

    Fix: Actualizada importación de @astrojs/vercel para evitar warning de deprecación

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://pintores-valencia.com',
