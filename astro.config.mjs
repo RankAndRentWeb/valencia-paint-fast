a2963e74da7f9f19808f4342ca74a3dc7794a046
Edgar7222 <1745554@uab.cat>
2025-10-07 13:58:41 +0200

    chore(git): ignore .vercel/ & remove vercel CLI from scripts; static astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://pintores-valencia.com',
   trailingSlash: 'always',
   output: 'static',
-  adapter: vercel(),
   integrations: [tailwind(), sitemap(), react()],
   markdown: {
     remarkPlugins: [
